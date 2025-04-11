1ddbe1301c9230659e7a44ba98e776383a0d14f0
Matan <meshulammatt@gmail.com>
2025-04-11 13:56:07 +0300

    adjusted files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,83 +1,77 @@
--- telescope.nvim
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-        -- build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release  && cmake  --build build --config Release",
-			},
-		},
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					fzf = {
-						fuzzy = true, -- false will only do exact matching
-						override_generic_sorter = true, -- override the generic sorter
-						override_file_sorter = true, -- override the file sorter
-						case_mode = "smart_case",
-					},
-				},
-				pickers = {
-					colorscheme = {
-						enable_preview = true,
-					},
-					find_files = {
-						hidden = true,
-						find_command = {
-							"rg",
-							"--files",
-							"--glob",
-							"!{.git/*,.next/*,.svelte-kit/*,target/*,node_modules/*}",
-							"--path-separator",
-							"/",
-						},
-					},
-				},
-			})
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+    'andrew-george/telescope-themes',
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local transform_mod = require("telescope.actions.mt").transform_mod
 
-			require("telescope").load_extension("fzf")
-			require("telescope").load_extension("zoxide")
-			-- telescope setup
-			local builtin = require("telescope.builtin")
+    local trouble = require("trouble")
 
-			vim.keymap.set(
-				"n",
-				"<leader>jk",
-				"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
-				{}
-			)
-			vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<cr>", {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
-			vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, {})
-			vim.keymap.set("n", "<leader>ws", builtin.lsp_workspace_symbols, {})
-			vim.keymap.set("n", "<leader>fz", ":Telescope zoxide list<CR>", {})
-			vim.keymap.set("n", "<leader>fv", builtin.help_tags, {})
-		end,
-	},
-	{
-		"jvgrootveld/telescope-zoxide",
-		config = function() end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-					},
-				},
-			})
-			-- To get ui-select loaded and working with telescope, you need to call
-			-- load_extension, somewhere after setup function:
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+    local builtin = require("telescope.builtin")
+    -- create your custom action
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function()
+        trouble.toggle("quickfix")
+      end,
+    })
+
+    -- NOTE: Telescope Extensions
+    telescope.load_extension("fzf")
+    telescope.load_extension('themes') -- Telescope themes by Andrew George
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next,     -- move test to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+          },
+        },
+      },
+      -- config for telescope themes
+      extensions = {
+        themes = {
+          -- (boolean) -> show/hide previewer window
+          enable_previewer = true,
+          -- (boolean) -> enable/disable live preview
+          enable_live_preview = true,
+          persist = {
+            enabled = true,
+            path = vim.fn.stdpath("config") .. "/lua/colorscheme.lua"
+          }
+        },
+      }
+    })
+
+    -- NOTE: set keymaps
+    -- vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+    vim.keymap.set("n", "<leader>pr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+    -- vim.keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy search string in cwd" })
+    -- vim.keymap.set( "n", "<leader>pc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+    -- Find all Hipattern comment tags in telescope
+    -- vim.keymap.set("n", "<leader>pt", "<cmd>TodoTelescope<CR>", { desc = "Find all comment tags in current dir" })
+    -- Find word under cursor
+    -- vim.keymap.set("n", "<leader>pws", function() local word = vim.fn.expand("<cword>") builtin.grep_string({ search = word }) end, {desc = "Find word under the cursor"})
+    -- Find everywhere in my project where example vim.keymap.set was used
+    vim.keymap.set("n", "<leader>pWs",
+      function()
+        local word = vim.fn.expand("<cWORD>")
+        builtin.grep_string({ search = word })
+      end, { desc = "Find Connected Words under cursor" })
+    -- find help for a command or for something
+    -- vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+    -- Switch Themes with Telescope and Save persistence
+    vim.keymap.set("n", "<leader>ths", ":Telescope themes<CR>",
+      { noremap = true, silent = true, desc = "Theme Switcher" })
+  end,
 }
