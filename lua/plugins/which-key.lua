94d42ae1da11ff79170381a6b290eab862b70c16
Matan <meshulammatt@gmail.com>
2025-12-19 21:20:27 +0200

    adjusted which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,13 +1,10 @@
-return {
+{
   "folke/which-key.nvim",
-  enabled = true,
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
   opts = {
-    -- empty for default settings
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
   },
   keys = {
     {
