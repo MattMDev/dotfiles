14c29d56e6b92a8996123001821a5a3938235a2c
Matan <meshulammatt@gmail.com>
2025-12-20 13:16:58 +0200

    testing oil again

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -14,6 +14,5 @@ return {
 
   -- Open parent directory in current window
   vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 }
 }
