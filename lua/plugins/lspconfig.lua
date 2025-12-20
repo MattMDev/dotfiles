b32912270114b9191ebb489a7d002d1444f00ac6
Matan <meshulammatt@gmail.com>
2025-12-20 11:14:35 +0200

    fixed lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -46,8 +46,6 @@ return {
         end,
     },
   {
-    dependencies = { 'saghen/blink.cmp' },
-
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       
