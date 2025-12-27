f67c7ec59c3830f7e24eabb77d4a9b29f184f6d7
Matan <meshulammatt@gmail.com>
2025-12-27 17:51:40 +0200

    fixed zshrc symlink issue

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@
 CONF_PATH="${HOME}/.config/"
 mkdir -p "${CONF_PATH}/hypr"
 
+LOCAL_DIR=$(pwd)
+
 # copy hyperland config
 cp ./hyprland.conf "${CONF_PATH}/hypr/hyprland.conf"
 
 # apply symlinks
-ln -sf ./.zshrc ~/.zshrc
-ln -sf ./kitty/ "${CONF_PATH}/kitty"
-ln -sf ./wofi/ "${CONF_PATH}/wofi"
+ln -sf "${LOCAL_DIR}/.zshrc" ~/.zshrc
+ln -sf "${LOCAL_DIR}/kitty/" "${CONF_PATH}/kitty"
+ln -sf "${LOCAL_DIR}/wofi/" "${CONF_PATH}/wofi"
