f07bd1866c920be410af9eba91ca08358f7c630b
Matan <meshulammatt@gmail.com>
2026-02-13 16:04:46 +0200

    adjusted install script to only re-copy hyperland config

--- install.sh
+++ install.sh
@@ -4,16 +4,15 @@
 # Install yay
 
 CONF_PATH="${HOME}/.config/"
-LOCAL_DIR=$(pwd)
 
 # copy hyperland config
-rm -rf "${CONF_PATH}/hypr/"
-cp -r ./hypr/ "${CONF_PATH}/hypr"
+cp -r ./hypr/hyprland.conf "${CONF_PATH}/hypr"
+exit
 
 # apply symlinks
-ln -sf "${LOCAL_DIR}/.zshrc" ~/.zshrc
-ln -sf "${LOCAL_DIR}/kitty/" "${CONF_PATH}/kitty"
-ln -sf "${LOCAL_DIR}/wofi/" "${CONF_PATH}/wofi"
-ln -sf "${LOCAL_DIR}/wallpapers/" "${CONF_PATH}/wallpapers"
-ln -sf "${LOCAL_DIR}/dunst/" "${CONF_PATH}/dunst"
-ln -sf "${LOCAL_DIR}/wpaperd/" "${CONF_PATH}/wpaperd"
+# ln -sf "${LOCAL_DIR}/.zshrc" ~/.zshrc
+# ln -sf "${LOCAL_DIR}/kitty/" "${CONF_PATH}/kitty"
+# ln -sf "${LOCAL_DIR}/wofi/" "${CONF_PATH}/wofi"
+# ln -sf "${LOCAL_DIR}/wallpapers/" "${CONF_PATH}/wallpapers"
+# ln -sf "${LOCAL_DIR}/dunst/" "${CONF_PATH}/dunst"
+# ln -sf "${LOCAL_DIR}/wpaperd/" "${CONF_PATH}/wpaperd"
