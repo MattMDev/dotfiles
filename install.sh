d75e1c47253ead28d24ee1389b79ec1b95a67495
Matan <meshulammatt@gmail.com>
2026-03-02 23:57:41 +0200

    adjusted installation script to symlink qutebrowser greasemonkey scripts

--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ LOCAL_DIR=$(pwd)
 # ln -sf "${LOCAL_DIR}/wpaperd/" "${CONF_PATH}/wpaperd"
 # ln -sf "${LOCAL_DIR}/qutebrowser/" "${CONF_PATH}/qutebrowser"
 # ln -sf "${LOCAL_DIR}/opencode/opencode.json" "${CONF_PATH}/opencode/opencode.json"
+ln -sf "${LOCAL_DIR}/qutebrowser/greasemonkey" "${HOME}/.local/share/qutebrowser/greasemonkey"
 
 exit
 
