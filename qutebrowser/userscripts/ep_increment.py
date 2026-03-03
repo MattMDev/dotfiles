daaeda3730a5b65d184285aa14a6926bf38b0cd3
Matan <meshulammatt@gmail.com>
2026-03-04 00:09:03 +0200

    fixed next episode userscript

--- qutebrowser/userscripts/ep_increment.py
+++ qutebrowser/userscripts/ep_increment.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
+import os
 import sys
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 with open("/tmp/ep_increment.log", "a") as f:
-    f.write(f"Script invoked! Args: {sys.argv}\n")
+    f.write(f"=== Script invoked! Args: {sys.argv} ===\n")
 
-url = sys.argv[1] if len(sys.argv) > 1 else None
-if not url:
-    with open("/tmp/ep_increment.log", "a") as f:
-        f.write("No URL provided\n")
+if len(sys.argv) < 2:
+    fifo = os.environ.get('QUTE_FIFO')
+    if fifo:
+        with open(fifo, 'w') as f:
+            f.write("message-error Usage: ep_increment <url>\n")
     sys.exit(1)
 
 url = sys.argv[1]
+with open("/tmp/ep_increment.log", "a") as f:
+    f.write(f"URL: {url}\n")
+
 parsed = urlparse(url)
 params = parse_qs(parsed.query)
 
-if 'ep' in params:
-    params['ep'][0] = str(int(params['ep'][0]) + 1)
+if 'ep' not in params:
+    fifo = os.environ.get('QUTE_FIFO')
+    if fifo:
+        with open(fifo, 'w') as f:
+            f.write(f"message-error No 'ep' param found in URL - {url}\n")
+    with open("/tmp/ep_increment.log", "a") as f:
+        f.write(f"ERROR: No ep param in URL: {url}\n")
+    sys.exit(1)
+
+params['ep'][0] = str(int(params['ep'][0]) + 1)
 
 new_query = urlencode(params, doseq=True)
 new_url = urlunparse((parsed.scheme, parsed.netloc, parsed.path, parsed.params, new_query, parsed.fragment))
 
-print(f"DEBUG: New URL: {new_url}", file=sys.stderr)
-print(f"open -t {new_url}")
+with open("/tmp/ep_increment.log", "a") as f:
+    f.write(f"New URL: {new_url}\n")
+
+fifo = os.environ.get('QUTE_FIFO')
+if fifo:
+    with open("/tmp/ep_increment.log", "a") as f:
+        f.write(f"Writing URL to FIFO: {new_url}\n")
+    with open(fifo, 'w') as f:
+        f.write(f"open {new_url}\n")
