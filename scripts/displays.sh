0f1d634dd14e9090e2e05bad1ed8d323b2531099
Matan <meshulammatt@gmail.com>
2026-03-06 16:22:01 +0200

    fixed displays script

--- scripts/displays.sh
+++ scripts/displays.sh
@@ -5,13 +5,13 @@ input="$1"
 
 # pass input to displays function
 if [ "$input" = "pc" ]; then
-    hyprctl keyword monitor "DP-1,2560x1440@164.96Hz,0x0,1"
-    hyprctl keyword monitor "DP-3,disable"
-    hyprctl keyword monitor "HDMI-A-3,disable"
+    hyprctl keyword monitor "DP-1, 2560x1440@164.96Hz, 0x0, 1"
+    hyprctl keyword monitor "DP-3, disable"
+    hyprctl keyword monitor "HDMI-A-3, disable"
 else
-    hyprctl keyword monitor "DP-1,disable"
-    hyprctl keyword monitor "DP-3,disable"
-    hyprctl keyword monitor "HDMI-A-3,2560x1440@60,auto-center-left,2"
+    hyprctl keyword monitor "DP-1, disable"
+    hyprctl keyword monitor "DP-3, disable"
+    hyprctl keyword monitor "HDMI-A-3, 2560x1440@60, auto-center-left, 2"
 fi
 
 # kill and respawn quickshell bar
