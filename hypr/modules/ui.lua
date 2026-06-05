0ab6a0d4643222b9183c45b0b7b2dc0630db976b
Matan <meshulammatt@gmail.com>
2026-06-05 12:57:21 +0300

    removed opacity for inactive window

--- hypr/modules/ui.lua
+++ hypr/modules/ui.lua
@@ -25,7 +25,7 @@ hl.config({
 
 		-- Change transparency of focused and unfocused windows
 		active_opacity = 1.0,
-		inactive_opacity = 0.7,
+		inactive_opacity = 1.0,
 
 		shadow = {
 			enabled = true,
